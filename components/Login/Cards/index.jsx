8a1567058d0e60d9494c439b962a5c64988f71fc
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2022-12-29 21:11:33 -0300

    Complete

--- components/Login/Cards/index.jsx
+++ components/Login/Cards/index.jsx
@@ -1,5 +1,6 @@
 import "./index.css";
 import trash from "./../../../img/trash.png";
+import vazio from "./../../../img/NoCard.png";
 
 export const Cards = ({ card, setCard, filtredCard, setFiltredCards }) => {
   const removeCard = (cardToRemove) => {
@@ -13,37 +14,47 @@ export const Cards = ({ card, setCard, filtredCard, setFiltredCards }) => {
 
   return (
     <ul>
-      {(filtredCard ? filtredCard : card).map((cards, index) => {
-        return (
-          <>
-            <li
-              key={index}
-              style={{
-                borderLeft:
-                  cards.type === "Entrada"
-                    ? "3px solid #65DE01"
-                    : "3px solid #FA0101",
-              }}
-            >
-              <div className="cards-left">
-                <h2>{cards.description}</h2>
-                <span className="type">{cards.type}</span>
-              </div>
+      {filtredCard.length === 0 || card.length === 0 ? (
+        <>
+          <h1>Sem lançamentos no momento.</h1>
+          <img src={vazio} alt="Imagem vazia" />
+        </>
+      ) : (
+        (filtredCard ? filtredCard : card).map((cards, index) => {
+          return (
+            <>
+              <li
+                key={index}
+                style={{
+                  borderLeft:
+                    cards.type === "Entrada"
+                      ? "3px solid #65DE01"
+                      : "3px solid #FA0101",
+                }}
+              >
+                <div className="cards-left">
+                  <h2>{cards.description}</h2>
+                  <span className="type">{cards.type}</span>
+                </div>
 
-              <div className="cards-right">
-                <span className="price">
-                  {`R$ ${parseFloat(cards.price).toFixed(2)}`.replace(".", ",")}
-                </span>
-                <img
-                  onClick={() => removeCard(cards.id)}
-                  src={trash}
-                  alt="Lixeira"
-                />
-              </div>
-            </li>
-          </>
-        );
-      })}
+                <div className="cards-right">
+                  <span className="price">
+                    {`R$ ${parseFloat(cards.price).toFixed(2)}`.replace(
+                      ".",
+                      ","
+                    )}
+                  </span>
+                  <img
+                    onClick={() => removeCard(cards.id)}
+                    src={trash}
+                    alt="Lixeira"
+                  />
+                </div>
+              </li>
+            </>
+          );
+        })
+      )}
     </ul>
   );
 };
